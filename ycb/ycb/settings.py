e8d2f7ac57ee363d141df7bf4beb07b6fb12f9c5
Richard Kim <34328828+richdotkim@users.noreply.github.com>
2021-03-10 22:34:56 -0500

    Update settings.py

--- ycb/ycb/settings.py
+++ ycb/ycb/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nedeo4x)^&u9+vj)g+9f-x&4r&2fp8^8idc#lvfl8utstsey@x'
+SECRET_KEY = *************
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
